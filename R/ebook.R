d4d35576b38daf9a6b0c88f313e9aafd51b25448
Yihui Xie <xie@yihui.name>
2016-07-13 10:19:19 +0000

    version 0.1

---
+++ R/ebook.R
@@ -0,0 +1,206 @@
+#' The EPUB e-book format
+#'
+#' Convert a book to the EPUB format, which is is an e-book format supported by
+#' many readers, such as Amazon Kindle Fire and iBooks on Apple devices.
+#' @param fig_width,fig_height,dev,fig_caption Figure options (width, height,
+#'   the graphical device, and whether to render figure captions).
+#' @param number_sections Whether to number sections.
+#' @param toc,toc_depth Whether to generate a table of contents, and its depth.
+#' @param stylesheet A character vector of paths to CSS stylesheets to be
+#'   applied to the eBook.
+#' @param cover_image The path to a cover image.
+#' @param metadata The path to the EPUB metadata file.
+#' @param chapter_level The level by which the e-book is split into separate
+#'   \dQuote{chapter} files.
+#' @param epub_version Whether to use version 3 or 2 of EPUB.
+#' @param md_extensions A character string of Pandoc Markdown extensions.
+#' @param pandoc_args A vector of additional Pandoc arguments.
+#' @note Figure/table numbers cannot be generated if sections are not numbered
+#'   (\code{number_sections = FALSE}).
+#' @export
+epub_book = function(
+  fig_width = 5, fig_height = 4, dev = 'png', fig_caption = TRUE,
+  number_sections = TRUE, toc = FALSE, toc_depth = 3, stylesheet = NULL,
+  cover_image = NULL, metadata = NULL, chapter_level = 1,
+  epub_version = c('epub3', 'epub'), md_extensions = NULL, pandoc_args = NULL
+) {
+
+  epub_version = match.arg(epub_version)
+  args = c(
+    pandoc_args,
+    if (number_sections) '--number-sections',
+    if (toc) '--toc',
+    if (!missing(toc_depth)) c('--toc-depth', toc_depth),
+    if (!is.null(cover_image)) c('--epub-cover-image', cover_image),
+    if (!is.null(metadata)) c('--epub-metadata', metadata),
+    if (!missing(chapter_level)) c('--epub-chapter-level', chapter_level)
+  )
+  if (is.null(stylesheet)) css = NULL else {
+    css = rmarkdown::pandoc_path_arg(epub_css(stylesheet))
+    args = c(args, '--epub-stylesheet', css)
+  }
+
+  from_rmarkdown = getFromNamespace('from_rmarkdown', 'rmarkdown')
+  from = from_rmarkdown(fig_caption, md_extensions)
+
+  config = rmarkdown::output_format(
+    knitr = rmarkdown::knitr_options_html(fig_width, fig_height, NULL, FALSE, dev),
+    pandoc = rmarkdown::pandoc_options(epub_version, from, args, ext = '.epub'),
+    pre_processor = function(metadata, input_file, runtime, knit_meta, files_dir, output_dir) {
+      process_markdown(input_file, from, args, !number_sections)
+      NULL
+    },
+    post_processor = function(metadata, input, output, clean, verbose) {
+      unlink(css)
+      if (is.null(opts$get('output_dir'))) return(output)
+      output2 = output_path(output)
+      file.rename(output, output2)
+      output2
+    }
+  )
+  config$bookdown_output_format = 'epub'
+  config = set_opts_knit(config)
+  config
+}
+
+process_markdown = function(input_file, from, pandoc_args, global) {
+  intermediate_html = with_ext(input_file, 'tmp.html')
+  on.exit(unlink(intermediate_html), add = TRUE)
+  rmarkdown::pandoc_convert(
+    input_file, 'html', from, intermediate_html, TRUE, c(pandoc_args, '--section-divs')
+  )
+  x = readUTF8(intermediate_html)
+  figs = parse_fig_labels(x, global)
+  # resolve cross-references and update the Markdown input file
+  content = resolve_refs_md(
+    readUTF8(input_file), c(figs$ref_table, parse_section_labels(x))
+  )
+  content = restore_part_epub(content)
+  content = restore_appendix_epub(content)
+  content = protect_math_env(content)
+  writeUTF8(content, input_file)
+}
+
+resolve_refs_md = function(content, ref_table) {
+  ids = names(ref_table)
+  # replace (\#fig:label) with Figure x.x:
+  for (i in grep('^(<p class="caption|<caption>|Table:)|(!\\[.*?\\]\\(.+?\\))', content)) {
+    for (j in ids) {
+      if (grepl(j, content[i], fixed = TRUE)) {
+        type = ifelse(grepl('^fig:', j), 'Figure', 'Table')
+        content[i] = sub(
+          sprintf('\\(\\\\#%s\\)', j),
+          sprintf('<span id="%s"></span>%s %s: ', j, type, ref_table[j]),
+          content[i]
+        )
+        break
+      }
+    }
+  }
+  # remove labels in figure alt text (it will contain \ like (\#fig:label))
+  content = gsub('"\\(\\\\#(fig:[-[:alnum:]]+)\\)', '"', content)
+
+  # look for \@ref(label) and resolve to actual figure/table/section numbers
+  m = gregexpr('(?<!`)\\\\@ref\\(([-:[:alnum:]]+)\\)', content, perl = TRUE)
+  refs = regmatches(content, m)
+  regmatches(content, m) = lapply(refs, ref_to_number, ref_table, TRUE)
+  content
+}
+
+reg_part = '^# \\(PART\\) .+ \\{-\\}$'
+
+# simply remove parts in epub
+restore_part_epub = function(x) {
+  x[grep(reg_part, x)] = ''
+  x
+}
+
+# this is not good enough since appendix chapters will continue to be numbered
+# after the last chapter instead of being numbered differently like A.1, A.2,
+# ..., but probably not too many people care about it in e-books
+restore_appendix_epub = function(x) {
+  r = '^(# )\\(APPENDIX\\) (.+ \\{-\\})$'
+  i = find_appendix_line(r, x)
+  if (length(i) == 0) return(x)
+  x[i] = gsub(r, '\\1\\2', x[i])
+  x
+}
+
+# wrap math environments in $$, otherwise they are discarded by Pandoc
+# https://github.com/jgm/pandoc/issues/2758
+protect_math_env = function(x) {
+  env = 'equation'  # may add more LaTeX environments later
+  s1 = sprintf('\\begin{%s}', env)
+  s2 = sprintf('\\end{%s}', env)
+  for (s in s1) {
+    i = x == s
+    x[i] = paste0('$$', x[i])
+  }
+  for (s in s2) {
+    i = x == s
+    x[i] = paste0(x[i], '$$')
+  }
+  x
+}
+
+# manually base64 encode images in css: https://github.com/jgm/pandoc/issues/2733
+epub_css = function(files, output = tempfile('epub', fileext = '.css')) {
+  css = unlist(lapply(files, function(css) {
+    in_dir(dirname(css), base64_css(basename(css)))
+  }))
+  writeUTF8(css, output)
+  output
+}
+
+#' A wrapper function to convert e-books using Calibre
+#'
+#' This function calls the command \command{ebook-convert} in Calibre
+#' (\url{http://calibre-ebook.com}) to convert e-books.
+#' @param input The input filename.
+#' @param output The output filename or extension (if only an extenion is
+#'   provided, the output filename will be the input filename with its extension
+#'   replaced by \code{output}; for example, \code{calibre('foo.epub', 'mobi')}
+#'   generates \file{foo.mobi}).
+#' @param options A character vector of additional options to be passed to
+#'   \command{ebook-convert}.
+#' @export
+#' @return The output filename.
+calibre = function(input, output, options = '') {
+  if (!grepl('[.]', output)) output = with_ext(input, output)
+  if (input == output) stop('input and output filenames are the same')
+  unlink(output)
+  system2('ebook-convert', c(shQuote(input), shQuote(output), options))
+  if (!file.exists(output)) stop('Failed to convert ', input, ' to ', output)
+  invisible(output)
+}
+
+#' A wrapper function to convert EPUB to the Mobipocket format
+#'
+#' This function simply calls the command line tool \command{kindlegen} provided
+#' by Amazon to convert EPUB e-books to the Mobipocket format (\file{.mobi}).
+#' @param epub The path to a \code{.epub} file (e.g. created from the
+#'   \code{\link{epub_book}()} format). If missing, it is automatically guessed
+#'   from the book configurations.
+#' @param exec The path to the executable \command{kindlegen}, which can be
+#'   downloaded from
+#'   \url{http://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211}.
+#' @return The path of the \file{.mobi} file if the conversion is successful.
+#' @export
+kindlegen = function(epub, exec = Sys.which('kindlegen')) {
+  if (exec == '') stop(
+    'Cannot find the executable KindleGen. You may download it from ',
+    'http://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211'
+  )
+  if (missing(epub)) {
+    on.exit(opts$restore(), add = TRUE)
+    config = load_config()
+    main = with_ext(book_filename(config), 'epub')
+    epub = file.path(output_dirname(NULL, config), main)
+  }
+  if (!file.exists(epub)) stop('The EPUB file ', epub, ' does not exist')
+  mobi = with_ext(epub, 'mobi')
+  unlink(mobi)
+  system2(exec, shQuote(epub))
+  if (!file.exists(mobi)) stop('Failed to convert epub to mobi')
+  mobi
+}
---
+++ R/latex.R
@@ -0,0 +1,113 @@
+#' Convert R Markdown to a PDF book
+#'
+#' Convert R Markdown files to PDF while resolving the special tokens of
+#' \pkg{bookdown} (e.g., the tokens for references and labels) to native LaTeX
+#' commands.
+#'
+#' This function is based on \code{rmarkdown::\link{pdf_document}} (by default)
+#' with better default arguments. You can also change the default format to
+#' other LaTeX/PDF format functions using the \code{base_format} argument.
+#' @param toc,number_sections,fig_caption See
+#'   \code{rmarkdown::\link{pdf_document}}, or the documentation of the
+#'   \code{base_format} function.
+#' @param ... Other arguments to be passed to \code{base_format}.
+#' @param base_format An output format function to be used as the base format.
+#' @export
+pdf_book = function(
+  toc = TRUE, number_sections = TRUE, fig_caption = TRUE, ...,
+  base_format = rmarkdown::pdf_document
+) {
+  base_format = get_base_format(base_format)
+  config = base_format(
+    toc = toc, number_sections = number_sections, fig_caption = fig_caption, ...
+  )
+  config$pandoc$ext = '.tex'
+  post = config$post_processor  # in case a post processor have been defined
+  config$post_processor = function(metadata, input, output, clean, verbose) {
+    if (is.function(post)) output = post(metadata, input, output, clean, verbose)
+    f = with_ext(output, '.tex')
+    x = resolve_refs_latex(readUTF8(f))
+    x = restore_part_latex(x)
+    x = restore_appendix_latex(x)
+    writeUTF8(x, f)
+    latexmk(f, config$pandoc$latex_engine)
+    unlink(with_ext(output, 'bbl'))  # not sure why latexmk left a .bbl there
+
+    output = with_ext(output, '.pdf')
+    o = opts$get('output_dir')
+    keep_tex = isTRUE(config$pandoc$keep_tex)
+    if (!keep_tex) file.remove(f)
+    if (is.null(o)) return(output)
+
+    output2 = file.path(o, output)
+    file.rename(output, output2)
+    if (keep_tex) file.rename(f, file.path(o, f))
+    output2
+  }
+  # always enable tables (use packages booktabs, longtable, ...)
+  pre = config$pre_processor
+  config$pre_processor = function(...) {
+    c(if (is.function(pre)) pre(...), '--variable', 'tables=yes', '--standalone')
+  }
+  config$bookdown_output_format = 'latex'
+  config = set_opts_knit(config)
+  config
+}
+
+#' @rdname html_document2
+#' @export
+pdf_document2 = function(...) {
+  pdf_book(..., base_format = rmarkdown::pdf_document)
+}
+
+#' @rdname html_document2
+#' @export
+tufte_handout2 = function(...) {
+  pdf_book(..., base_format = tufte::tufte_handout)
+}
+
+#' @rdname html_document2
+#' @export
+tufte_book2 = function(...) {
+  pdf_book(..., base_format = tufte::tufte_book)
+}
+
+resolve_refs_latex = function(x) {
+  x = gsub(
+    '(?<!\\\\textbackslash{})@ref\\(([-:[:alnum:]]+)\\)', '\\\\ref{\\1}', x,
+    perl = TRUE
+  )
+  x = gsub('\\(\\\\#((fig|tab):[-[:alnum:]]+)\\)', '\\\\label{\\1}', x)
+  x
+}
+
+restore_part_latex = function(x) {
+  r = '^\\\\chapter\\*\\{\\(PART\\) '
+  i = grep(r, x)
+  if (length(i) == 0) return(x)
+  x[i] = gsub(r, '\\\\part{', x[i])
+  # remove the line \addcontentsline since it is not really a chapter title
+  j = grep('\\addcontentsline{toc}{chapter}{(PART) ', x[i + 1], fixed = TRUE)
+  x[(i + 1)[j]] = ''
+  x
+}
+
+restore_appendix_latex = function(x) {
+  r = '^\\\\chapter\\*\\{\\(APPENDIX\\) '
+  i = find_appendix_line(r, x)
+  if (length(i) == 0) return(x)
+  x[i] = '\\appendix'
+  if (grepl('^\\\\addcontentsline', x[i + 1])) x[i + 1] = ''
+  x
+}
+
+find_appendix_line = function(r, x) {
+  i = grep(r, x)
+  if (length(i) > 1) stop('You must not have more than one appendix title')
+  i
+}
+
+latexmk = function(...) {
+  FUN = getFromNamespace('latexmk', 'rmarkdown')
+  FUN(...)
+}
